32edbedbc25864500f8d93c8bd91b8970c0dc0f1
PJ Dalton <patrickjdalton1@gmail.com>
2021-02-06 13:52:53 -0600

    Conditional handling of empty datasets in component sections

--- src/components/Player.js
+++ src/components/Player.js
@@ -6,11 +6,13 @@ function Player(props) {
         return (<li>{h}</li>)
     });
 
+    let hobbySection = (hobbies.length > 0 ? <ul>{hobbies}</ul> : <p class="red">no hobbies</p>);
+
     return (
         <div class="player">
         <h3>{playerName}</h3>
         <p>Jersey Number: {props.jerseyNumber}</p>
-        <ul>{hobbies}</ul>
+        {hobbySection}
         </div>
     )
 }
